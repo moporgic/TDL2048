76a110fc7e3c0c96d27397029df72eb9e74c713d
moporgic <hguei.cs03g@g2.nctu.edu.tw>
2017-02-17 15:47:59 +0800

    add 5x4patt predefine

--- 2048.cpp
+++ 2048.cpp
@@ -1121,6 +1121,7 @@ u32 make_weights(const std::string& res = "") {
 	predefined["4x6patt"] = predefined["khyeh"];
 	predefined["5x6patt"] = predefined["patt/42-33"];
 	predefined["8x6patt"] = predefined["k.matsuzaki"];
+	predefined["5x4patt"] = predefined["patt/4-22"];
 	for (auto predef : predefined) {
 		if (in.find(predef.first) != std::string::npos) { // insert predefined weights
 			in.insert(in.find(predef.first), predef.second);
@@ -1199,6 +1200,7 @@ u32 make_features(const std::string& res = "") {
 	predefined["4x6patt"] = predefined["khyeh"];
 	predefined["5x6patt"] = predefined["patt/42-33"];
 	predefined["8x6patt"] = predefined["k.matsuzaki"];
+	predefined["5x4patt"] = predefined["patt/4-22"];
 	for (auto predef : predefined) {
 		if (in.find(predef.first) != std::string::npos) { // insert predefined features
 			in.insert(in.find(predef.first), predef.second);
