cd0767f25ee83abbf5db2b8f9474986098acf4ad
moporgic <hguei.cs03g@g2.nctu.edu.tw>
2017-05-16 20:14:46 +0800

    improve statistic, add constructor and improve summary function

--- 2048.cpp
+++ 2048.cpp
@@ -1452,6 +1452,7 @@ struct statistic {
 	std::string indexf;
 	std::string localf;
 	std::string totalf;
+	std::string summaf;
 
 	struct control {
 		u64 loop;
@@ -1476,7 +1477,7 @@ struct statistic {
 		std::array<u64, 32> count;
 	} every;
 
-
+	statistic() : limit(0), loop(0), unit(0), winv(0), total({}), local({}), every({}) {}
 	void init(const control& ctrl = control()) {
 		limit = ctrl.loop * ctrl.unit;
 		loop = 1;
@@ -1490,6 +1491,7 @@ struct statistic {
 		indexf = "%0" + std::to_string(dec) + "llu/%0" + std::to_string(dec) + "llu %llums %.2fops";
 		localf = "local:" + std::string((dec << 1) - 4, ' ') + "avg=%llu max=%u tile=%u win=%.2f%%";
 		totalf = "total:" + std::string((dec << 1) - 4, ' ') + "avg=%llu max=%u tile=%u win=%.2f%%";
+		summaf = "summary" + std::string((dec << 1) - 5, ' ') + "%llums %.2fops";
 
 		every = {};
 		total = {};
@@ -1513,11 +1515,11 @@ struct statistic {
 
 		if ((loop % unit) != 0) return;
 
-		u64 currtimept = moporgic::millisec();
-		u64 elapsedtime = currtimept - local.time;
+		u64 current_time = moporgic::millisec();
+		local.time = current_time - local.time;
 		total.score += local.score;
 		total.win += local.win;
-		total.time += elapsedtime;
+		total.time += local.time;
 		total.opers += local.opers;
 		total.hash |= local.hash;
 		total.max = std::max(total.max, local.max);
@@ -1527,8 +1529,8 @@ struct statistic {
 		snprintf(buf, sizeof(buf), indexf.c_str(), // "%03llu/%03llu %llums %.2fops",
 				loop / unit,
 				limit / unit,
-				elapsedtime,
-				local.opers * 1000.0 / elapsedtime);
+				local.time,
+				local.opers * 1000.0 / local.time);
 		std::cout << buf << std::endl;
 		snprintf(buf, sizeof(buf), localf.c_str(), // "local:  avg=%llu max=%u tile=%u win=%.2f%%",
 				local.score / unit,
@@ -1544,12 +1546,19 @@ struct statistic {
 		std::cout << buf << std::endl;
 
 		local = {};
-		local.time = currtimept;
+		local.time = current_time;
 	}
 
 	void summary() const {
-		std::cout << std::endl << "summary" << std::endl;
 		char buf[80];
+		snprintf(buf, sizeof(buf), summaf.c_str(), total.time, total.opers * 1000.0 / total.time);
+		std::cout << std::endl << buf << std::endl;
+		snprintf(buf, sizeof(buf), totalf.c_str(), // "total:  avg=%llu max=%u tile=%u win=%.2f%%",
+				total.score / limit,
+				total.max,
+				math::msb32(total.hash),
+				total.win * 100.0 / loop);
+		std::cout << buf << std::endl;
 		snprintf(buf, sizeof(buf), "%-6s"  "%8s"    "%8s"    "%8s"   "%9s"   "%9s",
 								   "tile", "count", "score", "move", "rate", "win");
 		std::cout << buf << std::endl;
