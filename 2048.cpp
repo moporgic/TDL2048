199a5d38def590c4e13254b57aa64717c550e031
Hung Guei <hguei@moporgic.info>
2018-03-21 15:50:35 +0800

    add typedef signatures and split optimize(updv --> error/alpha)

--- 2048.cpp
+++ 2048.cpp
@@ -1446,7 +1446,8 @@ void list_mapping() {
 	}
 }
 
-
+typedef numeric(*estimator)(const board&, clip<feature>);
+typedef numeric(*optimizer)(const board&, numeric, numeric, clip<feature>);
 
 inline numeric estimate(const board& state,
 		clip<feature> range = feature::feats()) {
@@ -1456,8 +1457,9 @@ inline numeric estimate(const board& state,
 	return esti;
 }
 
-inline numeric optimize(const board& state, numeric updv,
+inline numeric optimize(const board& state, numeric error, numeric alpha,
 		clip<feature> range = feature::feats()) {
+	register numeric updv = alpha * error;
 	register numeric esti = 0;
 	for (register feature& feat : range)
 		esti += (feat[state] += updv);
@@ -1498,7 +1500,7 @@ struct state {
 	}
 	inline numeric optimize(numeric accu, numeric alpha = state::alpha(),
 			clip<feature> range = feature::feats()) {
-		esti = state::reward() + utils::optimize(move, alpha * (accu - state::value()), range);
+		esti = state::reward() + utils::optimize(move, accu - state::value(), alpha, range);
 		return esti;
 	}
 
