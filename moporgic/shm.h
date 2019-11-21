b26204c49e3c618b45157c7b153a7f9f4b48ccd3
Hung Guei <hguei@moporgic.info>
2019-11-21 12:53:24 +0800

    upgrade moporgious library (fix potential double free)

--- moporgic/shm.h
+++ moporgic/shm.h
@@ -17,16 +17,13 @@
 
 namespace moporgic {
 class shm {
-public:
-	static inline bool enable() { return flag(); }
-private:
-	static inline bool& flag() { static bool use = support(); return use; }
 #if defined(__linux__) && !defined(NOSHM)
 public:
-	static inline constexpr bool support() { return true; }
-	static inline void enable(bool use) { flag() = use; }
+	static constexpr bool support() { return true; }
+	static void enable(bool use) { if (!use) clear(); shm::use() = use; }
+	static void auto_cleanup(bool use) { shm::cleanup() = use; }
 
-	template<typename type> static inline type* alloc(size_t size) {
+	template<typename type = void> static type* alloc(size_t size) {
 		static uint8_t seq = 0;
 		static std::string hook = ({
 			std::string path = ".";
@@ -47,20 +44,14 @@ public:
 		return (type*) shm;
 	}
 
-	static inline void free(void* shm) {
+	template<typename type = void> static void free(type* shm) {
 		shmdt(shm);
 		shmctl(info().at(shm), IPC_RMID, nullptr);
 		info().erase(shm);
 	}
 
-public:
-	static __attribute__((constructor)) void init() {
-		signal(SIGINT, shm::interrupt_handler);
-	//	signal(SIGSEGV, [](int i) { std::exit(i); });
-	//	std::set_terminate([]() { clear(); __gnu_cxx::__verbose_terminate_handler(); });
-	//	std::atexit(shm::clear);
-	}
-	static __attribute__((destructor)) void clear() {
+	static void clear() {
+		if (&info(false) == nullptr) return;
 		for (auto blk : info()) {
 			shmdt(blk.first);
 			shmctl(blk.second, IPC_RMID, nullptr);
@@ -68,23 +59,43 @@ public:
 		info().clear();
 	}
 
+protected:
+	static __attribute__((constructor)) void init() {
+		signal(SIGINT, shm::interrupt_handler);
+	//	signal(SIGSEGV, [](int i) { std::exit(i); });
+	//	std::set_terminate([]() { clear(); __gnu_cxx::__verbose_terminate_handler(); });
+	//	std::atexit(shm::clear);
+	}
+	static __attribute__((destructor)) void exit() {
+		if (cleanup()) clear();
+		if (&info(false)) delete &info();
+	}
+
 	static void interrupt_handler(int i) {
-		clear();
+		if (cleanup()) clear();
 		std::quick_exit(i);
 	}
 
 private:
-	static std::map<void*, int>& info() {
-		static std::map<void*, int> i;
-		return i;
+	static std::map<void*, int>& info(bool try_init = true) {
+		static std::map<void*, int> *p = nullptr;
+		if (try_init && !p) p = new std::map<void*, int>;
+		return *p;
 	}
-#else
+#else /* if shm is not supported */
 public:
-	static inline constexpr bool support() { return false; }
-	static inline void enable(bool use) { if (use) throw std::invalid_argument("shm is not supported"); }
-	template<typename type> static inline type* alloc(size_t size) { throw std::bad_alloc(); }
-	static inline void free(void* shm) { throw std::bad_alloc(); }
+	static constexpr bool support() { return false; }
+	static void enable(bool use) { if (use) throw std::invalid_argument("shm is not supported"); }
+	static void auto_cleanup(bool use) { if (use) throw std::invalid_argument("shm is not supported"); }
+	template<typename type = void> static type* alloc(size_t size) { throw std::bad_alloc(); }
+	template<typename type = void> static void free(type* shm) { throw std::bad_alloc(); }
 #endif
+public:
+	static bool enable() { return shm::use(); }
+	static bool auto_cleanup() { return shm::cleanup(); }
+private:
+	static bool& use() { static bool use = support(); return use; }
+	static bool& cleanup() { static bool use = support(); return use; }
 };
 
 } // namespace moporgic
