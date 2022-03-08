91531a9d37ff9642f3274b6f6a9ab6a593a2c2df
Hung Guei <hguei@moporgic.info>
2022-03-08 14:56:14 +0800

    improve taskset with function subshell

--- utilities/2048-bench.sh
+++ utilities/2048-bench.sh
@@ -2,33 +2,36 @@
 # this script collects 2048 related benchmarking and testing procedures
 
 # default benchmarking kernel, will be invoked by "bench" and "compare"
-# note: this function is usually linked to a specialized function below
-test() { test-st "${@:-./2048}"; }
+# note: this function links to a specialized function set by variable "test"
+test() { ${test:-test-st} "${@:-./2048}"; }
 # specialized benchmarking kernels, should be wrapped with "test" before use
 # usage: test-[s|m]t [binary:./2048] [attempt:1x10000|$(nproc)0]{2} [thread:1|$(nproc)]
 #        test-[t|e]-[s|m]t [binary:./2048] [attempt:1x10000|$(nproc)0] [thread:1|$(nproc)]
-test-st() { (
-	run=$(name "${1:-./2048}")
-	invoke=${taskset:+taskset -c ${taskset[@]//;/,}}
-	$invoke $run -s -t ${2:-1x10000} -e $(echo-1st ${3} ${2/#0*/} 1x10000) -% | sum-ops
-) }
-test-mt() { (
-	run=$(name "${1:-./2048}")
-	tasks=(${taskset[@]//;/ }); tasks=(${tasks[@]:-""})
-	N_exec=$(($(nproc)0/${#tasks[@]}))
-	N_opti=${2:-$N_exec}
-	N_eval=$(echo-1st ${3} ${2/#0*/} $N_exec)
-	xsplit=-L${#tasks[@]}
-	(( ${N_opti/x*/} )) || unset N_opti xsplit
-	(( ${N_eval/x*/} )) || unset N_eval xsplit
-	{	for cores in ${tasks[@]:-""}; do
-			invoke=${cores:+taskset -c $cores}
-			N_proc=${4:-$($invoke nproc)}
-			$invoke $run -s ${N_opti:+-t $N_opti} ${N_eval:+-e $N_eval} -p $N_proc -% &
+test-st() (
+	run=$(runas "${1:-./2048}")
+	taskset -cp $(<<< ${taskset[@]} tr "; " ,) $BASHPID >/dev/null
+	opti=(${2} 1x${ratio:-10}000)
+	eval=(${3} ${2/#0*/} 1x${ratio:-10}000)
+	$run -s -t $opti -e $eval -% | grep summary | egrep -o [0-9.]+ops
+)
+test-mt() (
+	run=$(runas "${1:-./2048}")
+	taskset -cp $(<<< ${taskset[@]} tr "; " ,) $BASHPID >/dev/null
+	taskset=($(<<< ${taskset[@]} tr ";" " "))
+	taskset=(${taskset[@]:-""})
+	num=$(($(nproc)*${ratio:-10}/${#taskset[@]}))
+	opti=(${2} $num)
+	eval=(${3} ${2/#0*/} $num)
+	xsplit=-L${#taskset[@]}
+	(( ${opti/x*/} )) || unset opti xsplit
+	(( ${eval/x*/} )) || unset eval xsplit
+	{	for cores in ${taskset[@]:-""}; do
+			taskset -pc $cores $BASHPID >/dev/null
+			$run -s ${opti:+-t $opti} ${eval:+-e $eval} -p $(nproc) -% &
 		done; wait
-	} | sum-ops | xargs $xsplit | sed -e "s/ /+/g" -e "s/ops//g" | \
-		xargs printf "scale=2;%s;\n" | bc -l | sed -e "s/$/ops/g" | grep .
-) }
+	} | grep summary | egrep -o [0-9.]+ops | xargs $xsplit | sed -e "s/ /+/g" -e "s/ops//g" | \
+		xargs printf "scale=2;%s;\n" | bc -l | xargs -I% echo %ops | grep .
+)
 test-t-st() { test-st "${1:-./2048}" ${2:-1x10000} 0; }
 test-e-st() { test-st "${1:-./2048}" 0 ${2:-1x10000}; }
 test-t-mt() { test-mt "${1:-./2048}" ${2:-""} 0 ${3}; }
@@ -37,8 +40,8 @@ test-e-mt() { test-mt "${1:-./2048}" 0 ${2:-""} ${3}; }
 # benchmarking routine
 # usage: bench [binary:./2048] [attempt:10]
 # note: kernel function "test" should be defined before use
-bench () { (
-	run=$(name "${1:-./2048}")
+bench () (
+	run=$(runas "${1:-./2048}")
 	unset ops0 ops1
 	for i in $(seq -w 1 1 ${2:-10}); do
 		echo -n "#$i: "
@@ -52,17 +55,16 @@ bench () { (
 	echo -n ">$(sed "s/./>/g" <<< $i)> "
 	for ops in $ops0 $ops1; do
 		<<< "scale=2;(${ops})/${2:-10}" bc -l
-	done | sed "s/$/ops/g" | tr '\n' ' ' | grep .
-) }
+	done | xargs -I% echo %ops | xargs | grep .
+)
 
 # comparing two binaries
 # usage: compare [binary1:./2048] [binary2:./2048] [attempt:10]
 # note: kernel function "test" should be defined before use
-compare() { (
-	Lc=$(name "${1:-./base}")
-	Rc=$(name "${2:-./2048}")
-	Lx=0
-	Rx=0
+compare() (
+	Lc=$(runas "${1:-./base}")
+	Rc=$(runas "${2:-./2048}")
+	Lx=0 Rx=0
 	for i in $(seq -w 1 1 ${3:-10}); do
 		echo -n "#$i: "
 		if (( ${i: -1} % 2 == 0 )); then
@@ -82,25 +84,22 @@ compare() { (
 	echo -n ">$(sed "s/./>/g" <<< $i)> "
 	echo $(<<< "scale=2;(${Lx})/${3:-10}" bc -l)ops \
 	     $(<<< "scale=2;(${Rx})/${3:-10}" bc -l)ops | grep .
-) }
+)
 
 # full benchmarking routine
 # usage: benchmark [binary:./2048]...
 # note: kernel functions "bench" and "test-*" should be defined before use
 #       this procedure will automatically bind "test-*" as "test" for "bench"
 #       configurable variables: recipes, networks, threads, order, taskset, N_init, N_load
-benchmark() { (
-	PID=$BASHPID
-	tasksav=$(taskset -cp ${taskset[@]//;/,} $PID | head -n1 | cut -d':' -f2 | xargs)
-	taskset=${taskset:-$tasksav}
-
+benchmark() (
 	echo "TDL2048+ Benchmark @ $(hostname) @ ${when:=$(date +'%F %T')}"
+	taskset -cp $(<<< ${taskset[@]} tr "; " ,) $BASHPID >/dev/null
 	envinfo | stdbuf -o0 sed "s/^/# /g"
 
 	recipes=(${@:-${recipes[@]:-2048}})
 	networks=(${networks[@]:-4x6patt 8x6patt})
 	threads=(${threads[@]:-single multi})
-	order=(${order:-network thread recipe})
+	order=(${order[@]:-network thread recipe})
 	N_init=${N_init:-4}
 	N_load=${N_load:-2}
 
@@ -120,66 +119,62 @@ benchmark() { (
 
 	for token in ${tokens[@]}; do
 		token=(${token//_/ })
-		unset recipe network thread
 		declare ${order[0]}=${token[0]} ${order[1]}=${token[1]} ${order[2]}=${token[2]}
 
-		runas=$(name "${recipe:?}")
 		echo "[${recipe:?}] ${network:?} ${thread:?}-thread"
 		echo -n ">"
 
+		run=$(runas "${recipe:?}")
+		run_init="$run -n $network"
+		run_load="$run -n $network -i $network.w -a 0"
+
 		if (( $N_init )); then
-			test() { test-${thread:0:1}t "$1 -n $network" ${@:2}; }
-			bench $runas $N_init | tail -n1 | egrep -o [0-9.][0-9.]+ops | xargs echo -n ""
+			test="test-${thread:0:1}t" \
+				bench "$run_init" $N_init | tail -n1 | egrep -o [0-9.][0-9.]+ops | xargs echo -n ""
 			sleep 1
 		fi
 		if (( $N_load )) && [ -e $network.w ]; then
-			test() { test-${thread:0:1}t "$1 -n $network -i $network.w -a 0" ${@:2}; }
-			bench $runas $N_load | tail -n1 | egrep -o [0-9.][0-9.]+ops | xargs echo -n ""
+			test="test-${thread:0:1}t" \
+				bench "$run_load" $N_load | tail -n1 | egrep -o [0-9.][0-9.]+ops | xargs echo -n ""
 			sleep 1
 
-			test() { test-e-${thread:0:1}t "$1 -n $network -i $network.w -a 0" ${@:2}; }
-			bench $runas $N_load | tail -n1 | egrep -o [0-9.][0-9.]+ops | xargs echo -n ""
+			test="test-e-${thread:0:1}t" \
+				bench "$run_load" $N_load | tail -n1 | egrep -o [0-9.][0-9.]+ops | xargs echo -n ""
 			sleep 1
 		fi
 		echo
 	done
-
-	taskset -cp $tasksav $PID >/dev/null
-) }
+)
 
 # output the correct executable for command line
-name() { (
+runas() (
 	2048() { return 1; }
 	run="$@"
 	for run in "$run" "./$run" ""; do
 		$run -\| -n none -e 0 && break
 	done >/dev/null 2>&1
-	unset 2048
 	echo ${run:?\'$@\'}
-) }
-# extract ops from summary block
-sum-ops() { grep summary | egrep -o [0-9.]+ops; }
-# echo the 1st argument
-echo-1st() { echo "$1"; }
+)
 # measure CPU performance in GHz
-cpu-perf() { (
+cpu-perf() (
 	$(ls ./2048* | head -n1) -n 4x6patt -t ${1:-1}00 -p ${1:-1} >/dev/null &
 	sleep ${2:-5}
 	speed=$(grep MHz /proc/cpuinfo | cut -d':' -f2 | cut -b2- | sort -n | tail -n${1:-1})
 	pkill -P $(jobs -p)
 	kill $(jobs -p)
 	printf "%.1f\n" $(<<< "(${speed//$'\n'/+})/$(<<< $speed wc -l)000" bc -l)
-) }
+)
 # display the current environment
-envinfo() { (
+envinfo() (
 	# CPU model
 	cpuinfo=$(grep -m1 name /proc/cpuinfo | sed -E 's/.+:|\(\S+\)|CPU|[0-9]+-Core.+|@.+//g' | xargs)
 	nodes=$(lscpu | grep 'NUMA node(s)' | cut -d: -f2 | xargs)
 	(( ${nodes:-1} > 1 )) && cpuinfo+=" x$nodes"
 	# available cores
-	nproc=($(for cpus in ${taskset[@]//;/ }; do echo $(taskset -c $cpus nproc)x; done))
-	nproc=$(<<< ${nproc[@]:-$(nproc)x} tr ' ' '|')
-	[[ ${taskset[@]} ]] && nproc+=" (${taskset[@]// /;})"
+	taskset=$(<<< ${taskset[@]} tr ' ' ';')
+	nproc=($(for cpus in ${taskset//;/ }; do echo $(taskset -c $cpus nproc)x; done))
+	nproc=$(<<< ${nproc[@]:-$(taskset -c ${taskset//;/,} nproc)x} tr ' ' '|')
+	[[ $taskset ]] && nproc+=" ($taskset)"
 	# CPU speed
 	perf=$(cpu-perf 1)G-$(cpu-perf $(nproc))G
 	# memory info
@@ -216,7 +211,7 @@ envinfo() { (
 	when=${when:=$(date +'%F %T')}
 
 	echo "$commit @ $osinfo + $gccinfo @ $when"
-) }
+)
 
 # ======================================== main routine ========================================
 # check whether is running as benchmark or as source
@@ -225,7 +220,7 @@ envinfo() { (
 if (( $# + ${#recipes} )) && [ "$0" == "$BASH_SOURCE" ]; then ( # execute benchmarks
 	while (( $# )); do
 		case $1 in
-		-c*) taskset=${1:2}; taskset=${taskset/=/}; ;;
+		-c*) taskset=${1:2}; taskset=${taskset#=}; ;;
 		-*)  options+=${1:1}; ;;
 		*)   recipes+=${recipes:+ }$1; ;;
 		esac; shift
