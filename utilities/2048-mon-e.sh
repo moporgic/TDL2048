49e56672509c306e552d307175948e6c869445b3
Hung Guei <hguei@moporgic.info>
2022-04-12 00:11:11 +0800

    allow to read from stdin

--- utilities/2048-mon-e.sh
+++ utilities/2048-mon-e.sh
@@ -5,20 +5,29 @@
 # This tool fetches the results from summary block of log files and display them
 # The typical scenarios of using this tool are, when performing testing with different networks or different depths
 
-name=${1:-*}
-[[ $1 == *.x ]] || name=$name*.x
-for x in $(find $(dirname ${1:-.}) -name "$name" | sort); do
-	label="$(basename $x): "
-	[[ $1 ]] && label=$(<<<${label#$1} sed -E "s/^[:. -]+//g")
-	len=$((${#label} > len ? ${#label} : len))
-	L+=("$label")
-	R+=("$(grep summary -A20 $x | grep -E "^(total|${2:-32768})" | sed -E "s/${2:-32768}.+ / /g" | \
-		sed -E "s/.+avg=/+/g" | sed -E "s/ max=.+//g" | xargs | tr '+' ';' | sed "s/ ;/;/g" | cut -b2-)")
-done 2>/dev/null
+result() {
+	grep summary -A20 | grep -E "^(total|${win:-32768})" | sed -E "s/${win:-32768}.+ / /g" | \
+		sed -E "s/.+avg=/+/g" | sed -E "s/ max=.+//g" | xargs | tr '+' ';' | sed "s/ ;/;/g" | cut -b2- 2>/dev/null
+}
+
+if [[ $1 ]]; then # fetch from file(s)
+	[[ $1 == *.x ]] && name="$1" || name="$1*.x"
+	for x in $(find $(dirname ${1:-.}) -name "$name" | sort); do
+		label="$(basename $x): "
+		[[ $1 ]] && label=$(<<<${label#$1} sed -E "s/^[:. -]+//g")
+		len=$((${#label} > len ? ${#label} : len))
+		L+=("$label")
+		R+=("$(result < $x)")
+	done
+else # fetch from stdin
+	L=("")
+	R=("$(result)")
+	filter=${filter:-cat}
+fi
 for (( i=0; i<${#L[@]}; i++ )); do
 	label=${L[$i]}
 	blank=$(<<<$label tr '[:graph:]' ' ')
-	<<<${R[$i]} tr ';' '\n' | ${more:-tail -n1} | while IFS= read res; do
+	<<<${R[$i]} tr ';' '\n' | ${filter:-tail -n1} | while IFS= read res; do
 		printf "%-${len}s%6s%7s\n" "$label" $res
 		label=$blank
 	done
--- utilities/2048-mon-t.sh
+++ utilities/2048-mon-t.sh
@@ -6,6 +6,7 @@
 # The typical scenarios of using this tool are, when performing a long-term training with periodic evaluation,
 # with all statistics are written into a single log file
 
-res=$(grep summary -A 1 ${1%.x}.x | grep 'avg=' | cut -d':' -f2 | xargs -L1)
+[[ $1 ]] && src=${1%.x}.x || unset src
+res=$(grep summary -A 1 $src | grep 'avg=' | cut -d':' -f2 | xargs -L1)
 idx=$(($(<<<$res wc -l) - 1))
 <<<$res nl -v0 -w${#idx} -s': ' -nrz
