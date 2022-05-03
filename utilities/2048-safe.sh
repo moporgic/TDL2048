5a0135dff10f64f2bb6804857639cc9b6e470a99
Hung Guei <hguei@moporgic.info>
2022-05-03 22:37:39 +0800

    increase default threshold to 20000

--- utilities/2048-safe.sh
+++ utilities/2048-safe.sh
@@ -5,4 +5,4 @@
 # This tool extract score from the latest log (avg=*) and check whether it is below the threshold
 # The score is extracted from the last summary block (if it presents); or from the last normal statistic block
 
-{ tail -n20 "${1:--}" | grep ops -A 1 || echo =0; } | tail -n1 | cut -d'=' -f2 | cut -d' ' -f1 | xargs -I{} [ {} -ge ${thres:-10000} ]
+{ tail -n20 "${1:--}" | grep ops -A 1 || echo =0; } | tail -n1 | cut -d'=' -f2 | cut -d' ' -f1 | xargs -I{} [ {} -ge ${thres:-20000} ]
