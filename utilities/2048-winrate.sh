8830665200ad3ab9537431b092b25ba8588abee6
Hung Guei <hguei@moporgic.info>
2022-07-17 19:05:36 +0800

    fix incorrect and missing output for some extreme cases

--- utilities/2048-winrate.sh
+++ utilities/2048-winrate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#History: 2019-06-15 moporgic
+# History: 2019-06-15 moporgic
+#          2022-07-17 moporgic
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
 values=()
@@ -14,7 +15,7 @@ tiles=${tiles[@]}
 tiles=${tiles//,/|}
 tiles=${tiles// /|}
 
-tiles=$((
+tiles=$({
 	egrep -o '[0-9]+' <<< "$tiles"
 	for range in $(egrep -o '[0-9]+\|\.\.\.\|[0-9]+'); do
 		[[ $range =~ ([0-9]+)[^0-9]+([0-9]+) ]]
@@ -24,42 +25,33 @@ tiles=$((
 			[ $tile -ge $lower ] && [ $tile -le $upper ] && echo $tile
 		done
 	done <<< "2|$tiles|65536"
-) | sort -h | uniq | egrep ${values// /|})
+} | sort -h | uniq | egrep ${values// /|} | xargs)
 
-tiles=${tiles//$'\n'/|}
-echo "${tiles//|/[%]$'\t'}[%]"
-egrep "^($tiles) +[0-9]+ +[0-9]+ +[0-9]+ +[0-9.]+% +[0-9.]+%$" | awk -v raw="$tiles" '
-BEGIN {
-	n = split(raw, tiles, "|");
-	expect = 1;
-}
-{
-	tile = $1;
-	win = $6;
-	
-	while (tile < tiles[expect]) {
-		printf "0.00%%";
-		expect = expect % n + 1;
-		printf (expect != 1 ? "\t" : "\n");
-	}
-	
-	while (tile != tiles[expect]) {
-		printf "100.00%%\t";
-		expect++;
-	}
-	
-	printf "%s", win;
-	expect = expect % n + 1;
-	printf (expect != 1 ? "\t" : "\n");
-}
-END {
-	if (!tile) {
-		for (i = 1; i < n; i++) printf "100.00%%\t";
-		printf "100.00%%\n";
-	}
-	while (expect != 1) {
-		printf "0.00%%";
-		expect = expect % n + 1;
-		printf (expect != 1 ? "\t" : "\n");
-	}
-}'
+echo "${tiles// /[%]$'\t'}[%]"
+
+{ egrep "^[0-9]+ [0-9. ]+% +[0-9.]+%$" | while IFS= read -r res; do
+	tile=${res%% *}
+	win=${res##* }
+	[ "$win" == "100.00%" ] && echo
+	echo -n "$tile:$win "
+done | tail -n+2; echo; } | while IFS= read -r result; do
+	[[ $result ]] || continue
+	declare -A stat
+
+	tile=2
+	win="100.00%"
+	for res in $result; do
+		for (( this=${res%:*}; tile<this; tile+=tile )); do
+			stat[$tile]=$win
+		done
+		win=${res#*:}
+	done
+	stat[$tile]=$win
+	for (( tile=tile*2; tile<=65536; tile+=tile )); do
+		stat[$tile]="0.00%"
+	done
+
+	for tile in $tiles; do
+		echo ${stat[$tile]}
+	done | xargs | tr ' ' '\t'
+done
