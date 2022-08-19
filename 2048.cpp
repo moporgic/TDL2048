a4486ad06dc42791b36f1c10e01c0762ecef3c9f
Hung Guei <hguei@moporgic.info>
2022-08-20 00:12:08 +0800

    refactor expectimax search_best

--- 2048.cpp
+++ 2048.cpp
@@ -1647,14 +1647,14 @@ struct method {
 		}
 
 		static inline numeric search_best(const board& before, u32 depth, clip<feature> range = feature::feats()) {
-			numeric best = 0;
-			u32 step = 0;
+			numeric best = 0, expt, esti;
 			for (const board& after : before.moves<board>()) {
 				if (after.info() == -1u) continue;
-				best = std::max(best, after.info() + search_expt(after, depth - 1, range));
-				step = 1;
+				expt = search_expt(after, depth - 1, range);
+				esti = after.info() + std::max(expt, numeric(0));
+				best = std::max(best, esti + 1);
 			}
-			return best + step;
+			return best;
 		}
 
 		static inline numeric estimate(const board& after, clip<feature> range = feature::feats()) {
