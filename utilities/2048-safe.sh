2698ddbaa98d3123a3262d5a123a70195a47059a
Hung Guei <hguei@moporgic.info>
2023-07-27 20:43:28 +0800

    use -1 as default score for detection; remove useless $thres variable

--- utilities/2048-safe.sh
+++ utilities/2048-safe.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Program: Check for network corruption from a log file
+# 2048-safe.sh: Check for training failures from the log file
 
 # This tool extract score from the latest log (avg=*) and check whether it is below the threshold
 # The score is extracted from the last summary block (if it presents); or from the last normal statistic block
 
-score=$({ tail -n20 "${1:--}" | grep ops -A 1 || echo =0; } | tail -n1 | cut -d'=' -f2 | cut -d' ' -f1)
-(( score >= ${2:-${thres:-100000}} && score <= ${3:-2000000} ))
+score=$({ tail -n20 "${1:--}" | grep ops -A 1 || echo =-1; } | tail -n1 | cut -d'=' -f2 | cut -d' ' -f1)
+(( score >= ${2:-100000} && score <= ${3:-2000000} ))
